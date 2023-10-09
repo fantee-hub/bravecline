7ac2dfa497354af8ff84ff1f09f66c07d4ea8eec
Tobi Faniran <tobifaniran@gmail.com>
2023-10-09 15:59:35 +0100

    feat: hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="bg-hero-bg bg-no-repeat w-full h-screen 4xl:h-[60vh] flex items-center bg-cover text-white px-10">
+      <div className="max-w-[60rem] flex flex-col gap-5">
+        <div className="text-5xl font-bold">STUDY ABROAD</div>
+        <div className="text-3xl leading-[1.3]">
+          Studying abroad applications used to be a complex process.
+          <br /> We assist with eligibility criteria, course selection, and
+          application procedures.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,29 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      ssm: "400px",
+
+      smm: "500px",
+
+      sm: "640px",
+
+      md: "775px",
+
+      lg: "998px",
+
+      xl: "1280px",
+
+      "1.5xl": "1400px",
+
+      "2xl": "1500px",
+
+      "3xl": "1600px",
+
+      "4xl": "1700px",
+
+      "5xl": "1800px",
+    },
     fontFamily: {
       copperGothic: ["var(--copper-gothic)", "sans-serif"],
     },
