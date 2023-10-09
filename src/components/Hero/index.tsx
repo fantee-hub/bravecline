0f5201ae435b39cfa29aab30f60cd0946a973644
Tobi Faniran <tobifaniran@gmail.com>
2023-10-09 16:32:39 +0100

    feat: the study abroad section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="bg-hero-bg bg-no-repeat w-full h-screen 4xl:h-[60vh] flex items-center bg-cover text-white px-10">
+    <div className="bg-hero-bg bg-no-repeat w-full h-screen 4xl:h-[50vh] 3xl:h-[60vh] flex items-center bg-cover text-white px-10">
       <div className="max-w-[60rem] flex flex-col gap-5">
         <div className="text-5xl font-bold">STUDY ABROAD</div>
         <div className="text-3xl leading-[1.3]">
--- src/components/Study/index.tsx
+++ src/components/Study/index.tsx
@@ -1,11 +1,29 @@
 import React from "react";
+import abroad from "../../../public/assets/study-abroad.png";
 
 const Study = () => {
   return (
-    <section className="py-10">
+    <section className="pt-10 bg-slate-400 bg-opacity-5 pb-20">
       <div className="font-caveat text-5xl text-center">
         <div>STUDY ABROAD</div>
         <div>Are you willing to futher your studies abroad?</div>
+      </div>
+      <div className="shadow-lg max-w-[50rem] w-full mx-auto">
+        <div className="flex flex-col justify-center mt-10">
+          <img src={abroad.src} alt="study abroad" />
+          <div className="py-5 px-4 flex flex-col gap-5">
+            <div>
+              Our expert team has simplified the process for you. Discover your
+              dream educational journey we assist with eligibility criteria,
+              course selection, and application procedures.
+            </div>
+            <div>
+              <button className="bg-slate-900 rounded text-white outline-none p-1">
+                READ MORE
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
