52ad7105ec4cb870c5b60e9b53fad58af90c4fa8
Tobi Faniran <tobifaniran@gmail.com>
2023-10-09 16:58:19 +0100

    added a utils file

---
+++ src/utils/utils.ts
@@ -0,0 +1,20 @@
+export const packages = [
+  {
+    id: crypto.randomUUID(),
+    img: "/assets/image1.png",
+    title: "PASSPORT and VISA",
+    desc: "Travel confidently with a valid passport. Reach out to us today for all your passport and travel documentation including VISA needs we're here to make your travel experience seamless. Whether you're applying for a new passport or need a renewal our dedicated team is at your service.",
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "/assets/image2.png",
+    title: "IMMIGRATION PROCESSING",
+    desc: "Immigrating to Canada through the Express Entry systemr each out to us for assistance at +2348063224937 or visit www.BRAVECALINE.com. Before this system, immigration applications for Canada used to be managed through the Federal Skilled Worker Program. The introduction of the Express Entry immigration program in January 2015 has significantly sped up the process.",
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "/assets/image3.png",
+    title: "TICKETING",
+    desc: "You can reserve your airline tickets through our services. Our partnerships and access to global flight booking systems allow us to secure seats for our clients on almost any flight to any destination worldwide. Our expertise lies in arranging flights for both economy and business class travelers,catering to both leisure and business travel needs.",
+  },
+];
