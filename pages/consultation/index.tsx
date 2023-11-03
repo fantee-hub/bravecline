fc4232e303210297bc459ed49dab0c3280ddf084
Tobi Faniran <tobifaniran@gmail.com>
2023-11-03 23:41:33 +0100

    feat: added the consultation page and section

---
+++ pages/consultation/index.tsx
@@ -0,0 +1,12 @@
+import ConsultationSection from "@/src/components/ConsultationSection";
+import React from "react";
+
+const Consultation = () => {
+  return (
+    <div>
+      <ConsultationSection />
+    </div>
+  );
+};
+
+export default Consultation;
