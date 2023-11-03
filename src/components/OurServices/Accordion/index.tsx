c9671389602c0dc4425f8949d4015c5b1444baaf
Tobi Faniran <tobifaniran@gmail.com>
2023-11-03 08:28:19 +0100

    feat: new updates

--- src/components/OurServices/Accordion/index.tsx
+++ src/components/OurServices/Accordion/index.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Collapse } from "react-collapse";
+import { PiCaretDownBold, PiCaretUpBold } from "react-icons/pi";
 
-const Accordion = () => {
-  return <div>Accordion</div>;
+const Accordion = ({
+  children,
+  currentOpen,
+  setCurrentOpen,
+  num,
+  title,
+  btn,
+}: any) => {
+  const isOpen = num === currentOpen;
+  const handleToggle = () => {
+    setCurrentOpen(isOpen ? false : num);
+  };
+  return (
+    <div
+      onClick={handleToggle}
+      className={`lg:p-8 p-4 cursor-pointer ${
+        isOpen
+          ? "bg-gray-50 rounded-2xl border border-gray-200"
+          : " bg-white border-b border-gray-200"
+      }`}
+    >
+      <div className="flex justify-between lg:gap-3 gap-0 relative">
+        <div className="text-gray-900 lg:text-xl text-sm font-semibold sm:max-w-[38rem] max-w-[17rem]">
+          {title}
+        </div>
+        <div className="w-10 h-10 bg-gray-50 rounded-full flex items-center justify-center">
+          {!isOpen ? <PiCaretDownBold /> : <PiCaretUpBold />}
+          {/* <img src={!isOpen ? plus.src : minus.src} alt="" /> */}
+        </div>
+      </div>
+      <Collapse isOpened={isOpen}>
+        <div className="text-slate-600 lg:text-lg text-sm leading-[28px] pt-2">
+          {children}
+        </div>
+        <div className="pt-5">
+          <button className="h-12 bg-slate-900 flex items-center justify-center rounded-lg outline-none text-zinc-100 px-4 lg:text-base text-sm">
+            {btn}
+          </button>
+        </div>
+      </Collapse>
+    </div>
+  );
 };
 
 export default Accordion;
--- src/components/OurServices/index.tsx
+++ src/components/OurServices/index.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { faqs } from "@/src/utils/utils";
+import Accordion from "./Accordion";
 
 const OurServices = () => {
-  return <div>OurServices</div>;
+  const [currentOpen, setCurrentOpen] = useState(false);
+  return (
+    <section className="max-w-[1472px] 2xl:mx-auto lg:px-10 px-4 mx-auto relative pt-10 pb-20">
+      <div className="font-caveat lg:text-5xl text-xl text-center">
+        <div>Our Service</div>
+        <div className="lg:text-3xl text-base">
+          Our free international study application supports includes:
+        </div>
+      </div>
+      <div className="flex justify-center items-center mt-14 gap-10 lg:flex-row flex-col">
+        <div className="flex flex-col gap-2 pt-5 lg:pt-0 flex-1">
+          {faqs.map((item, index) => (
+            <Accordion
+              key={item.id}
+              currentOpen={currentOpen}
+              setCurrentOpen={setCurrentOpen}
+              num={index}
+              title={item.title}
+              btn={item.buttonText}
+            >
+              {item.answer}
+            </Accordion>
+          ))}
+        </div>
+        <div className="flex-1">
+          <img src="/assets/consult-illustration.jpeg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default OurServices;
