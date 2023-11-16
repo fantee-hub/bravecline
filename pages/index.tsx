9a273254f242355cee901f234355f274e20094c7
Tobi Faniran <tobifaniran@gmail.com>
2023-11-16 07:40:22 +0100

    feat: new changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,29 @@ import Head from "next/head";
 export default function Home() {
   return (
     <main>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"
+        />
+
+        <title>Bravecline</title>
+        <meta
+          name="description"
+          content="Studying abroad applications used to be a complex process"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#000000"></meta>
+        <meta property="og:title" content="Bravecline" />
+        <meta property="og:url" content="https://bravecline.vercel.app/" />
+        <meta
+          property="og:description"
+          content="Studying abroad applications used to be a complex process"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
       <Hero />
       <Study />
       <Packages />
